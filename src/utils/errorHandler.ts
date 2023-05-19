e3325ea9baa545334b2d99fbc7ccb23609569794
Wilson Fabozi <wilsonfabozi@gmail.com>
2023-05-19 07:26:55 -0300

    feat: :sparkles: utils functions

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,1 +1,9 @@
-// TODO: fazer aqui
+import logger from '../logger';
+
+const errorHandler = async(error: any, label: string, mongo = false) => {
+  logger('error', error, label, mongo);
+
+  Promise.resolve();
+};
+
+export default errorHandler;
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export const labelGenerator = () => new Date().getTime().toString();
